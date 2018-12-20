99937db59028cb8db37ed9624366f9e6237ea9b8
crisa <cgazzola@student.bridgew.edu>
2018-12-20 14:36:10 -0500

    Added solution to Minimal Tree Problem

---
+++ src/MinimalTree.java
@@ -0,0 +1,31 @@
+import TreesAndGraphs.BinaryTree;
+import TreesAndGraphs.BinaryTreeNode;
+
+public class MinimalTree extends BinaryTree {
+
+    public static void main(String[] args) {
+
+        int[] arr = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        MinimalTree minimalTree = new MinimalTree();
+        BinaryTreeNode root = minimalTree.createMinimalBST(arr);
+        minimalTree.inOrderTraversal(root);
+    }
+
+    BinaryTreeNode createMinimalBST(int[] arr) {
+        return createMinimalBST(arr, 0, arr.length - 1);
+    }
+
+    BinaryTreeNode createMinimalBST(int[] arr, int start, int end) {
+        if (end < start) {
+            return null;
+        }
+
+        int mid = (start + end) / 2;
+        BinaryTreeNode node = new BinaryTreeNode(arr[mid]);
+        node.setLeft(createMinimalBST(arr, start, mid - 1));
+        node.setRight(createMinimalBST(arr, mid + 1, end));
+        return node;
+    }
+
+}
--- src/TreesAndGraphs/BinaryTree.java
+++ src/TreesAndGraphs/BinaryTree.java
@@ -56,37 +56,3 @@ public class BinaryTree {
 
 
 }
-
-class BinaryTreeNode {
-    private int val;
-    private BinaryTreeNode left;
-    private BinaryTreeNode right;
-
-    public BinaryTreeNode(int val) {
-        this.val = val;
-        this.left = null;
-        this.right = null;
-    }
-
-    public void setLeft(BinaryTreeNode left) {
-        this.left = left;
-    }
-
-    public void setRight(BinaryTreeNode right) {
-        this.right = right;
-    }
-
-    public BinaryTreeNode getLeft() {
-        return left;
-    }
-
-    public BinaryTreeNode getRight() {
-        return right;
-    }
-
-    public int getVal() {
-        return val;
-    }
-
-
-}
