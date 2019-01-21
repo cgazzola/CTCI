ca19cb3e5f3223257ab6dd2e5f95811fd1939d8b
crisa <crisagazzola@aol.com>
2019-01-21 15:32:15 -0500

    Added a recursive, bubble up solution to the LCA problem

--- src/TreesAndGraphs/LCA.java
+++ src/TreesAndGraphs/LCA.java
@@ -29,31 +29,45 @@ public class LCA {
 //        n13.setLeft(n7);
 //        n7.setParent(n13);
 
-        BinaryTreeNode root = new BinaryTreeNode(20);
-        BinaryTreeNode n10 = new BinaryTreeNode(10);
-        root.setLeft(n10);
-        n10.setParent(root);
+//        BinaryTreeNode root = new BinaryTreeNode(20);
+//        BinaryTreeNode n10 = new BinaryTreeNode(10);
+//        root.setLeft(n10);
+//        n10.setParent(root);
+//        BinaryTreeNode n5 = new BinaryTreeNode(5);
+//        n10.setLeft(n5);
+//        n5.setParent(n10);
+//        BinaryTreeNode n3 = new BinaryTreeNode(3);
+//        n5.setLeft(n3);
+//        n3.setParent(n5);
+//        BinaryTreeNode n7 = new BinaryTreeNode(7);
+//        n7.setParent(n5);
+//        n5.setRight(n7);
+//        BinaryTreeNode n15 = new BinaryTreeNode(15);
+//        n10.setRight(n15);
+//        n15.setParent(n10);
+//        BinaryTreeNode n17 = new BinaryTreeNode(17);
+//        n15.setRight(n17);
+//        n17.setParent(n15);
+//        BinaryTreeNode n30 = new BinaryTreeNode(30);
+//        root.setRight(n30);
+//        n30.setParent(root);
+
+        BinaryTreeNode root = new BinaryTreeNode(3);
+        BinaryTreeNode n1 = new BinaryTreeNode(1);
+        root.setLeft(n1);
         BinaryTreeNode n5 = new BinaryTreeNode(5);
-        n10.setLeft(n5);
-        n5.setParent(n10);
-        BinaryTreeNode n3 = new BinaryTreeNode(3);
-        n5.setLeft(n3);
-        n3.setParent(n5);
+        root.setRight(n5);
+        BinaryTreeNode n8 = new BinaryTreeNode(8);
+        n5.setRight(n8);
         BinaryTreeNode n7 = new BinaryTreeNode(7);
-        n7.setParent(n5);
-        n5.setRight(n7);
-        BinaryTreeNode n15 = new BinaryTreeNode(15);
-        n10.setRight(n15);
-        n15.setParent(n10);
-        BinaryTreeNode n17 = new BinaryTreeNode(17);
-        n15.setRight(n17);
-        n17.setParent(n15);
-        BinaryTreeNode n30 = new BinaryTreeNode(30);
-        root.setRight(n30);
-        n30.setParent(root);
 
         LCA lca = new LCA();
-        System.out.println(lca.commonAncestor3(root, n7, n17).getVal());
+        BinaryTreeNode ancestor = lca.commonAncestorRecursion(root, n5, n7);
+        if (ancestor != null) {
+            System.out.print(ancestor.getVal());
+        } else {
+            System.out.print("Either p or q does not exist in the tree");
+        }
 
     }
 
@@ -138,6 +152,54 @@ public class LCA {
         return ancestor3Helper(childSide, p, q);
     }
 
+    BinaryTreeNode commonAncestorRecursion(BinaryTreeNode root, BinaryTreeNode p, BinaryTreeNode q) {
+        Result r = commonAncHelper(root, p, q);
+        if (r.isAncestor) {
+            return r.node;
+        }
+        return null;
+    }
 
+    Result commonAncHelper(BinaryTreeNode root, BinaryTreeNode p, BinaryTreeNode q) {
+        if (root == null) {
+            return new Result(null, false);
+        }
+        if (root == p && root == q) {
+            return new Result(root, true);
+        }
+
+        Result rx = commonAncHelper(root.getLeft(), p, q);
+        if (rx.isAncestor) { // Found common ancestor
+            return rx;
+        }
+
+        Result ry = commonAncHelper(root.getRight(), p, q);
+        if (ry.isAncestor) { // Found common ancestor
+            return ry;
+        }
+
+        if (rx.node != null && ry.node != null) {
+            return new Result(root, true); // This is the common ancestor
+        } else if (root == p || root == q) {
+            /* If we're currently at p or qm and we also found one of those nodes in a
+            subtree, then this is truly an ancestor and the flag should be true
+             */
+            boolean isAncestor = rx.node != null || ry.node != null;
+            return new Result(root, isAncestor);
+        } else {
+            return new Result(rx.node != null ? rx.node : ry.node, false);
+        }
+    }
+
+}
+
+class Result {
+    public BinaryTreeNode node;
+    public boolean isAncestor;
+
+    public Result(BinaryTreeNode n, boolean isAnc) {
+        node = n;
+        isAncestor = isAnc;
+    }
 }
 
