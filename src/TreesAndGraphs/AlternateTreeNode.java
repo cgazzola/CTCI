cd1fc63ecd7e0a98e6e7c88912fd78aec2531acb
crisa <crisagazzola@aol.com>
2019-01-24 21:37:34 -0500

    Added first solution to the Random Node problem

--- src/TreesAndGraphs/AlternateTreeNode.java
+++ src/TreesAndGraphs/AlternateTreeNode.java
@@ -1,7 +1,64 @@
 package TreesAndGraphs;
 
-/**
- * Created by devmachine on 1/24/19.
- */
+import java.util.Random;
+
 public class AlternateTreeNode {
+    private int data;
+    public AlternateTreeNode left;
+    public AlternateTreeNode right;
+    private int size = 0;
+
+    public AlternateTreeNode(int d) {
+        data = d;
+        size = 1;
+    }
+
+    public AlternateTreeNode getRandomNode() {
+        int leftSize = left == null ? 0 : left.size();
+        Random random = new Random();
+        int index = random.nextInt(size);
+        if (index < leftSize) {
+            return left.getRandomNode();
+        } else if (index == leftSize) {
+            return this;
+        } else {
+            return right.getRandomNode();
+        }
+    }
+
+    public void insertInOrder(int d) {
+        if (d <= data) {
+            if (left == null) {
+                left = new AlternateTreeNode(d);
+            } else {
+                left.insertInOrder(d);
+            }
+        } else {
+            if (right == null) {
+                right = new AlternateTreeNode(d);
+            } else {
+                right.insertInOrder(d);
+            }
+        }
+        size++;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public int data() {
+        return data;
+    }
+
+    public AlternateTreeNode find(int d) {
+        if (d == data) {
+            return this;
+        } else if (d <= data) {
+            return left != null ? left.find(d) : null;
+        } else if (d > data) {
+            return right != null ? right.find(d) : null;
+        }
+        return null;
+    }
 }
--- src/TreesAndGraphs/RandomNode.java
+++ src/TreesAndGraphs/RandomNode.java
@@ -1,7 +1,26 @@
 package TreesAndGraphs;
 
-/**
- * Created by devmachine on 1/24/19.
- */
+import java.util.Random;
+
 public class RandomNode {
+
+    public static void main(String[] args) {
+        AlternateTreeNode root = new AlternateTreeNode(20);
+        root.insertInOrder(10);
+        root.insertInOrder(30);
+        root.insertInOrder(5);
+        root.insertInOrder(15);
+        root.insertInOrder(3);
+        root.insertInOrder(7);
+        root.insertInOrder(17);
+
+        System.out.println(root.getRandomNode().data());
+        System.out.println(root.getRandomNode().data());
+        System.out.println(root.getRandomNode().data());
+        System.out.println(root.getRandomNode().data());
+        System.out.println(root.getRandomNode().data());
+        System.out.println(root.getRandomNode().data());
+        System.out.println(root.getRandomNode().data());
+
+}
 }
