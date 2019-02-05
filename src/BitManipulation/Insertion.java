e725627af4eb6d0cb1c1bafc9279368fb82a08b6
crisa <crisagazzola@aol.com>
2019-02-05 18:49:29 -0500

    Added main method with parameters

--- src/BitManipulation/Insertion.java
+++ src/BitManipulation/Insertion.java
@@ -2,7 +2,16 @@ package BitManipulation;
 
 public class Insertion {
 
-    int updateBits(int n, int m, int i, int j) {
+    public static void main(String[] args) {
+        int n = Integer.parseInt("10000000000", 2);
+        int m = Integer.parseInt("10011", 2);
+
+        int x = updateBits(n, m, 2, 6);
+
+        System.out.println(Integer.toBinaryString(x));
+    }
+
+    static int updateBits(int n, int m, int i, int j) {
         /* Create a mask to clear bits i through j in n. Example: i = 2, j = 4.
         Result should be 11100011. For simplicity, we'll use just 8 bits for the example. */
 
@@ -19,6 +28,9 @@ public class Insertion {
 
         /* Clear bits j through i then put m in there */
         int n_cleared = n & mask; // Clear bits j through i
+
+        System.out.println(n_cleared);
+
         int m_shifted = m << i; // Move m into correct position.
 
         return n_cleared | m_shifted; // OR them, and we're done!
