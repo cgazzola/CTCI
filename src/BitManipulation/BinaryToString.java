9c316237767abe376ee37846b6364feb3d7c9ce7
crisa <crisagazzola@aol.com>
2019-02-05 20:36:14 -0500

    Added Solution to BinaryToString problem

---
+++ src/BitManipulation/BinaryToString.java
@@ -0,0 +1,34 @@
+package BitManipulation;
+
+public class BinaryToString {
+
+    public static void main(String[] args) {
+        double x = 0.625;
+        System.out.println(printBinary(x));
+    }
+
+    public static String printBinary(double num) {
+        if (num >= 1 || num <= 0) {
+            return "ERROR";
+        }
+
+        StringBuilder binary = new StringBuilder();
+        binary.append(".");
+        while (num > 0) {
+            /* Setting a limit on length: 32 characters */
+            if (binary.length() >= 32) {
+                return "ERROR";
+            }
+
+            double r = num * 2;
+            if (r >= 1) {
+                binary.append(1);
+                num = r - 1;
+            } else {
+                binary.append(0);
+                num = r;
+            }
+        }
+        return binary.toString();
+    }
+}
