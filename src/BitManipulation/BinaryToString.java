ff06e56a3640c10d90a183ffa2a82245b0b5a5d5
crisa <crisagazzola@aol.com>
2019-02-05 20:40:45 -0500

    Added alternative solution

--- src/BitManipulation/BinaryToString.java
+++ src/BitManipulation/BinaryToString.java
@@ -4,7 +4,7 @@ public class BinaryToString {
 
     public static void main(String[] args) {
         double x = 0.625;
-        System.out.println(printBinary(x));
+        System.out.println(printBinary2(x));
     }
 
     public static String printBinary(double num) {
@@ -28,6 +28,30 @@ public class BinaryToString {
                 binary.append(0);
                 num = r;
             }
+        }
+        return binary.toString();
+    }
+
+    public static String printBinary2(double num) {
+        if (num >= 1 || num <= 0) {
+            return "ERROR";
+        }
+
+        StringBuilder binary = new StringBuilder();
+        double frac = 0.5;
+        binary.append(".");
+        while (num > 0) {
+            /* Setting a limit on length: 32 characters */
+            if (binary.length() > 32) {
+                return "ERROR";
+            }
+            if (num >= frac) {
+                binary.append(1);
+                num -= frac;
+            } else {
+                binary.append(0);
+            }
+            frac /= 2;
         }
         return binary.toString();
     }
