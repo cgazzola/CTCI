450fc7eab5a7e6c35dec090586dea5069d7733b8
crisa <crisagazzola@aol.com>
2019-02-10 21:07:03 -0500

    Added getPrev method

--- src/BitManipulation/NextNumber.java
+++ src/BitManipulation/NextNumber.java
@@ -6,7 +6,7 @@ public class NextNumber {
 
         int num = 13948;
         System.out.println(Integer.toBinaryString(num));
-        System.out.println(Integer.toBinaryString(getNext(num)));
+        System.out.println(getPrev(num) + " -> " + Integer.toBinaryString(getPrev(num)));
     }
 
     static int getNext(int n) {
@@ -30,7 +30,30 @@ public class NextNumber {
         n &= ~((1 << p) - 1); // Clear all bits to the right of p
         n |= (1 << (c1 - 1)) - 1; // Insert (c1-1) ones on hte right
         return n;
+    }
 
+    static int getPrev(int n) {
+        int temp = n;
+        int c0 = 0;
+        int c1 = 0;
+        while ((temp & 1) == 1) {
+            c1++;
+            temp >>= 1;
+        }
 
+        if (temp == 0) return -1;
+
+        while (((temp & 1) == 0) && temp != 0) {
+            c0++;
+            temp >>= 1;
+        }
+
+        int p = c0 + c1; // position of rightmost non-trailing one
+        n &= ((~0) << (p + 1)); // clears from bit p onwards
+
+        int mask = (1 << (c1 + 1)) - 1; // Sequence of (c1+1) ones
+        n |= mask << (c0 - 1);
+
+        return n;
     }
 }
