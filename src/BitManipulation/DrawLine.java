933f13f6e5cff32fe73698b533426bc1765b212e
crisa <crisagazzola@aol.com>
2019-02-20 22:14:19 -0500

    Added solution to DrawLine problem

---
+++ src/BitManipulation/DrawLine.java
@@ -0,0 +1,50 @@
+package BitManipulation;
+
+public class DrawLine {
+
+    public static void main(String[] args) {
+        byte[] screen = new byte[64];
+        int width = 2;
+        int x1 = 7;
+        int x2 = 14;
+        drawLine(screen, width, x1, x2, 0);
+    }
+
+    public static void drawLine(byte[] screen, int width, int x1, int x2, int y) {
+        int start_offset = x1 % 8;
+        int first_full_byte = x1 / 8;
+        if (start_offset != 0) {
+            first_full_byte++;
+        }
+
+        int end_offset = x2 % 8;
+        int last_full_byte = x2 / 8;
+        if (end_offset != 7) {
+            last_full_byte--;
+        }
+
+        // Set full bytes
+        for (int b = first_full_byte; b <= last_full_byte; b++) {
+            screen[(width / 8) * y + b] = (byte) 0xFF;
+        }
+
+        // Create masks for start and end of line
+        byte start_mask = (byte) (0xFF >> start_offset);
+        byte end_mask = (byte) ~(0xFF >> (end_offset + 1));
+
+        // Set start and end of line
+        if ((x1 / 8) == (x2 / 8)) { // x1 and x2 are in the same byte
+            byte mask = (byte) (start_mask & end_mask);
+            screen[(width / 8) * y + (x1 / 8)] |= mask;
+        } else {
+            if (start_offset != 0) {
+                int byte_number = (width / 8) * y + first_full_byte - 1;
+                screen[byte_number] |= start_mask;
+            }
+            if (end_offset != 7) {
+                int byte_number = (width / 8) * y + last_full_byte + 1;
+                screen[byte_number] |= end_mask;
+            }
+        }
+    }
+}
