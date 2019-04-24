775753d55050babd2a7f6da1075cd1169bd7e934
crisa <crisagazzola@aol.com>
2019-04-24 07:00:21 -0400

    Added solution to Sorted Squares

--- src/LeetCode/SortedSquares.java
+++ src/LeetCode/SortedSquares.java
@@ -1,7 +1,44 @@
 package LeetCode;
 
-/**
- * Created by devmachine on 4/20/19.
- */
 public class SortedSquares {
+
+    public static void main(String[] args) {
+        int[] A = new int[]{-3, -2, -1, 4, 5, 6};
+
+        sortedSquares(A);
+    }
+
+
+    public static int[] sortedSquares(int[] A) {
+        int N = A.length;
+        int j = 0;
+        while (j < N && A[j] < 0)
+            j++;
+        int i = j - 1;
+
+        int[] ans = new int[N];
+        int t = 0;
+
+        while (i >= 0 && j < N) {
+            if (A[i] * A[i] < A[j] * A[j]) {
+                ans[t++] = A[i] * A[i];
+                i--;
+            } else {
+                ans[t++] = A[j] * A[j];
+                j++;
+            }
+        }
+
+        while (i >= 0) {
+            ans[t++] = A[i] * A[i];
+            i--;
+        }
+        while (j < N) {
+            ans[t++] = A[j] * A[j];
+            j++;
+        }
+
+        return ans;
+    }
 }
+
